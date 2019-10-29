f3ddb64eb528a8c95cb195f29a7a2e07ae0f6697
Elaine Onuegbe <elaine_onuegbe@epam.com>
2019-10-28 23:31:11 -0700

    file changes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,14 @@
-import {
-GET_ALL_GIFS
-} from '../constants'
+import { GET_ALL_GIFS } from '../constants';
 
-export const get_all_gifs_success = (gifs) => {
+export const get_all_gifs_success = gifs => {
   return {
     type: GET_ALL_GIFS,
     gifs,
-  }
-}
+  };
+};
 
 export const getGifsFromAPI = () => dispatch => {
   fetch('https://lit-dusk-44111.herokuapp.com/api/v1/gifs')
-  .then(resp => resp.json())
-  .then(data => dispatch(get_all_gifs_success(data)))
-}
- 
+    .then(resp => resp.json())
+    .then(data => dispatch(get_all_gifs_success(data)));
+};
--- src/containers/Homepage/Homepage.js
+++ src/containers/Homepage/Homepage.js
@@ -6,29 +6,30 @@ import TagFilter from '../../components/TagFilter/TagFilter';
 import './Homepage.scss';
 
 class Homepage extends Component {
-  componentDidMount(){
-    this.props.getGifsFromAPI()
+  componentDidMount() {
+    this.props.getGifsFromAPI();
   }
+
   render() {
-    const {gifs} = this.props;
+    const { gifs } = this.props;
     return (
       <>
         <div className="filter-container">
           <TagFilter />
         </div>
         <div className="list-container">
-          <HomeList
-          gifs={gifs}
-          />
+          <HomeList gifs={gifs} />
         </div>
       </>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   gifs: state,
 });
 
-
-export default connect(mapStateToProps, {getGifsFromAPI})(Homepage);
+export default connect(
+  mapStateToProps,
+  { getGifsFromAPI },
+)(Homepage);
